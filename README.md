3a661a71b24ec2d46f0bf856aa71beb7deeb5e01
Jose Perez <jose-perez-cs@hotmail.com>
2017-11-12 23:52:54 -0800

    Y

---
+++ README.md
@@ -0,0 +1,1 @@
+This is VideoGen Assignment for CSE202
---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "FrameRenderer.h"
+
+using namespace std;
+
+const double frames_per_second = 30; 
+const int duration_in_seconds = 3;
+
+int main(int argc, char * argv[]) {
+	// Construct the ffmpeg command to run.
+	FrameRenderer renderer;
+
+	int num_frames = duration_in_seconds * frames_per_second;
+	for (int i = 0; i < num_frames; ++i) {
+		double time_in_seconds = i / frames_per_second;
+		renderer.draw_frame(time_in_seconds, 0x00, 0xff, 0x00);
+	}
+
+	cout << "num_frames: " << num_frames << endl;
+	cout << "Done." << endl;
+
+	return 0;
+}
